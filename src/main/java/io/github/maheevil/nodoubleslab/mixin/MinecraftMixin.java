e77c145b4f37ae0504a6692f5aea21b0cbfb766a
Maheesha <maheeshasithum@gmail.com>
2023-03-22 23:51:10 +0530

    ACTION_RELEASE release time

--- src/main/java/io/github/maheevil/nodoubleslab/mixin/MinecraftMixin.java
+++ src/main/java/io/github/maheevil/nodoubleslab/mixin/MinecraftMixin.java
@@ -4,6 +4,9 @@ import io.github.maheevil.nodoubleslab.IOptionGetter;
 import io.github.maheevil.nodoubleslab.NoDoubleSlabPlacementMod;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.Options;
+import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.network.chat.Component;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -15,15 +18,24 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class MinecraftMixin {
     @Shadow @Final public Options options;
 
+    @Shadow @Nullable public LocalPlayer player;
+
     @Inject(
             method = "handleKeybinds",
             at = @At("RETURN")
     )
     private void handleKeybinds$NoDoubleSlabPlacement(CallbackInfo callbackInfo){
         while(((IOptionGetter) options).getNoSlabKey().consumeClick()){
+            boolean toggleValue = !NoDoubleSlabPlacementMod.getToggleValue();
             NoDoubleSlabPlacementMod.setToggleValue(
-                    !NoDoubleSlabPlacementMod.getToggleValue())
-            ;
+                    toggleValue
+            );
+            assert this.player != null;
+            this.player.displayClientMessage(
+                    Component.literal(
+                            toggleValue ? "No Double Slab Placement" : "Double Slab Placement Allowed"
+                    ), true
+            );
         }
     }
 }
